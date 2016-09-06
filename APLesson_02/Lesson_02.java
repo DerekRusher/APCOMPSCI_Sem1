b75002e4a161c0c38466123fc5be1eadbb69d406
DerekRusher <RusherD9652@sduhsd.lan>
2016-09-06 11:52:32 -0700

    Added Data Operators, and the Lesson_02 file class and folder

---
+++ APLesson_02/Lesson_02.java
@@ -0,0 +1,22 @@
+public class Lesson_02
+{
+	public static void main(String[]args)
+	{
+		int l = 8;
+		int w = 5;
+		int recArea = l * w;
+		
+		System.out.println("The Area of your rectagle is " + recArea + ".");
+		
+	}
+}
+
+//String name = "JOHN CENA";
+//System.out.println(name);
+//System.out.println(8+5);
+//int num1 = 9;
+//int num2 = 5;
+//System.out.println(num1 * num2);
+//String a = "JOHN ";
+//String b = "CENA";
+//System.out.println(a + b);
