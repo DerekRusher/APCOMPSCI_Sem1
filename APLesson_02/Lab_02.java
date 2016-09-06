280790d91c8fe15b5ae5e19ac71a3bc749b357ee
DerekRusher <RusherD9652@sduhsd.lan>
2016-09-06 12:14:08 -0700

    almost finished Lab_02

--- APLesson_02/Lab_02.java
+++ APLesson_02/Lab_02.java
@@ -8,9 +8,17 @@ public class Lab_02
 		
 		System.out.println(a + " multiplied by " + b + " is " + AB);
 		
-		String Name = 
-		String address = 
-		String City = 
-		String zip = 
+		String Name = "Martin Thomas";
+		String address = "413 Generic Street";
+		String City = "LOLAR";
+		String zip = "00612.";
+		
+		System.out.println(Name + " lives on " + address + " on " + City + ", " + zip);
+		
+		int Length = 6;
+		int Height = 7;
+		int Width = 8;
+		
+		System.out.println(Length);
 	}
 }
