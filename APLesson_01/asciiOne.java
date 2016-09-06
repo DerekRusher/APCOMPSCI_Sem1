0248e3e67a6dc48e42a202f0a48671a4ecc71fb0
DerekRusher <RusherD9652@sduhsd.lan>
2016-09-06 10:52:59 -0700

    Added Files

---
+++ APLesson_01/asciiOne.java
@@ -0,0 +1,16 @@
+public class asciiOne{
+	public static void main(String[]args)
+	{
+		System.out.println("                                                                   |");
+		System.out.println("                                                                   |\\");
+		System.out.println("                                                                   <<\\   -");
+		System.out.println("                                                                    / \\ //");
+		System.out.println(" _-------------------------------------------------------------{o}______/|");
+		System.out.println("<   -===================================================:::{*}///////////]");
+		System.out.println(" `-------------------------------------------------------------{o}~~~~~~\\|");
+		System.out.println("                                                                    \\ /  \\\\");
+		System.out.println("                                                                    <</   ~");
+		System.out.println("                                                                    |/");
+		System.out.println("                                                                    |");
+	}
+}
---
+++ APLesson_01/asciiTwo.java
@@ -0,0 +1,16 @@
+public class asciiTwo{
+	public static void main(String[]args)
+	{
+		System.out.println("                     .::.");
+		System.out.println("                  .:'  .:");
+		System.out.println("        ,MMM8&&&.:'   .:'");
+		System.out.println("       MMMMM88&&&&  .:'");
+		System.out.println("      MMMMM88&&&&&&:'");
+		System.out.println("      MMMMM88&&&&&&");
+		System.out.println("    .:MMMMM88&&&&&&");
+		System.out.println("  .:'  MMMMM88&&&&");
+		System.out.println(".:'   .:'MMM8&&&'");
+		System.out.println(":'  .:'");
+		System.out.println("'::'");
+	}
+}
