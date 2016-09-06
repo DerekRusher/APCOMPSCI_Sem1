abf9fc337f0f937b791fb8d0a86be07e3392b96b
DerekRusher <RusherD9652@sduhsd.lan>
2016-09-06 12:03:56 -0700

    renamed files, added Lab_02

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,16 @@
+public class Lab_02 
+{
+	public static void main(String[]args)
+	{
+		int a = 3;
+		int b = 7;
+		int AB = (3 * 7);
+		
+		System.out.println(a + " multiplied by " + b + " is " + AB);
+		
+		String Name = 
+		String address = 
+		String City = 
+		String zip = 
+	}
+}
