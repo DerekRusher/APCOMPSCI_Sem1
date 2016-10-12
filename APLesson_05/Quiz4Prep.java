85d0e4750d11c090a5222deb99a6a81a39b4000c
DerekRusher <derekrusher3@gmail.com>
2016-10-12 11:55:08 -0700

    copied over quiz prep for convineince

---
+++ APLesson_05/Quiz4Prep.java
@@ -0,0 +1,34 @@
+/*Question_01*/ (wrong [needs only one line]) would get right
+
+System.out.printf("The total cost of your order is $%.2f", one + two);
+
+/*Question_02*/ (wrong [put sum instead of price]) would get right
+
+System.out.printf("%11S .......... $%.2f", item, price);
+
+/*Question_03*/ (wrong [numPrint can’t be referenced from a static context])
+
+A.
+
+/*Question_04*/ (right, answer was [System.out.printf("The surface area of your cube is %.5f inches. ",<--
+--> calcSurf(side));])
+
+System.out.printf("The surface area of your cube is %.5f", side*side*side);
+
+/*Question_05*/ (right, I contest. answer was [System.out.printf("The sum of " + num1 + " and " +  num2<--
+--> + " is %.2f".format(calcSum(one, two));])
+
+double sum = one + two;
+System.out.printf("The sum of  t%f and %f is %.2f", one, two, sum);
+
+/*Question_06*/ (right, I contest. answer was [c])
+
+D.
+
+/*Question_07*/ (no answer)
+
+A.
+
+/*Question_08*/ (no answer)
+
+A.
