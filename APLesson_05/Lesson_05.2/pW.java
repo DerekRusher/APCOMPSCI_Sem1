5b8c3afd0cbbff6f1c43168694e9c29563f19ba9
DerekRusher <derekrusher3@gmail.com>
2016-10-20 10:36:42 -0700

    finished LAB 5.2

--- APLesson_05/Lesson_05.2/pW.java
+++ APLesson_05/Lesson_05.2/pW.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class pw
+public class pW
 {
 	public static void main(String[]args)
 	{
@@ -11,11 +11,11 @@ public class pw
 	
 	public static void check(String one, String two)
 	{
-		Scanner in = new Scanner;
+		Scanner in = new Scanner(System.in);
 		System.out.println("Please enter your username");
-		String a = in.next();
+		String a = in.nextLine();
 		System.out.println("Please enter your password");
-		String b = in.next();
+		String b = in.nextLine();
 		
 		if(a.equals(one) && b.equals(two))
 		{
@@ -23,7 +23,19 @@ public class pw
 		}
 		else if(a.equals(one))
 		{
-			System.out.println("IncorrectPW, please try again");
+			System.out.println("Incorrect password, please try again");
+			check(one, two);
 		}
+		else if(b.equals(two))
+		{
+			System.out.println("Incorrect username, please try again");
+			check(one, two);
+		}
+		else
+		{
+			System.out.println("Incorrect username and password, please try again");
+			check(one, two);
+		}
+		
 	}
 }
