fe4e362388c1e0f208dbc66f22a18398fcbe69b3
DerekRusher <derekrusher3@gmail.com>
2016-10-26 10:54:46 -0700

    did SOMETHING to gpaFinal and did the test prep for quiz 5

---
+++ TestPrep/Quiz5Prep.java
@@ -0,0 +1,72 @@
+//QUESTION 1:
+	
+if (one > two)
+{
+	double a = one - two;
+	if (a >= 1.7)
+	{
+		System.out.println("the bond is ionic");
+	}
+	else
+	{
+		System.out.println("the bond is covalent");
+	}
+}
+if (one < two)
+{
+	double a = two - one;
+	if (a >= 1.7)
+	{
+		System.out.println("the bond is ionic");
+	}
+	else
+	{
+		System.out.println("the bond is covalent");
+	}
+}
+else
+{
+	System.out.println("the bond is covalent");
+}
+
+//Question 2:
+D.
+
+//Question 3:
+D.
+
+//Question 4:
+A.
+
+//Question 5: 
+D.
+
+//Question 6:
+D.
+
+//Question 7:
+A.
+
+//Question 8: 
+C.
+
+//Question 9:
+C.
+
+//Question 10:
+if(total == MAX)
+	{
+		if (total < sum)
+		{
+			System.out.println("total == MAX and is < sum. ");
+		}
+		else
+		{
+			System.out.println("total == MAX and is >= sum. ");
+		}
+	}
+else
+   System.out.println("total is not equal to MAX");
+
+
+
