35b7be907cb42a4bde5e762a0c3428e005686526
DerekRusher <derekrusher3@gmail.com>
2016-10-28 11:17:40 -0700

    finishes gpa final! (unless i wanna class it up and make it NOT 275 lines of code)

--- Personal/gpaFinal.java
+++ Personal/gpaFinal.java
@@ -17,6 +17,7 @@ public class gpaFinal
 	static boolean b6;
 	static boolean b7;
 	static boolean b8;
+	static double count = 0;
 	public static void main(String[]args)
 	{
 		Scanner in = new Scanner(System.in);
@@ -82,32 +83,62 @@ public class gpaFinal
 				double tgwv2 = (tenerGPAVersion2+w+1)/13;
 					System.out.printf("Your wieghted GPA for 10th-11th grade (semesters not averaged) is %.1f or %f\n", tgwv2, tgwv2);
 			}
-		double ninerGPA = nineGPA();
-			double nguw = ninerGPA/19;
-				System.out.printf("Your unwieghted GPA for 9th-11th grade (semesters averaged) is %.1f or %f\n", nguw, nguw);
-			double ngw = (ninerGPA+w+1)/19;
-				System.out.printf("Your wieghted GPA for 9th-11th grade (semesters averaged) is %.1f or %f\n", ngw, ngw);
-		double ninerGPAVersion2 = nineGPAVersion2();
-			double nguwv2 = ninerGPAVersion2/19;
-				System.out.printf("Your unwieghted GPA for 9th-11th grade (semesters not averaged) is %.1f or %f\n", nguwv2, nguwv2);
-			double ngwv2 = (ninerGPAVersion2+w+1)/19;
-				System.out.printf("Your wieghted GPA for 9th-11th grade (semesters not averaged) is %.1f or %f\n", ngwv2, ngwv2);
-		double sevenerGPA = sevenGPA();
-			double sguw = sevenerGPA/22;
-				System.out.printf("Your unwieghted GPA for 7th-11th grade (semesters averaged) is %.1f or %f\n", sguw, sguw);
-			double sgw = (sevenerGPA+w+1)/22;
-				System.out.printf("Your wieghted GPA for 7th-11th grade (semesters averaged) is %.1f or %f\n", sgw, sgw);
-		double sevenerGPAVersion2 = sevenGPAVersion2();
-			double sguwv2 = sevenerGPAVersion2/22;
-				System.out.printf("Your unwieghted GPA for 7th-11th grade (semesters not averaged) is %.1f or %f\n", sguwv2, sguwv2);
+		}
+		if(b3 == true)
+		{
+			double ninerGPA = nineGPA();
+			double ninerGPAVersion2 = nineGPAVersion2();
+			if(b5 == true && b7 == true)
+			{
+				double nguw = ninerGPA/19;
+					System.out.printf("Your unwieghted GPA for 9th-11th grade (semesters averaged) is %.1f or %f\n", nguw, nguw);
+			}
+			if(b6 == true && b7 == true)
+			{
+				double ngw = (ninerGPA+w+1)/19;
+					System.out.printf("Your wieghted GPA for 9th-11th grade (semesters averaged) is %.1f or %f\n", ngw, ngw);
+			}
+			if(b5 == true && b8 == true)
+			{
+				double nguwv2 = ninerGPAVersion2/19;
+					System.out.printf("Your unwieghted GPA for 9th-11th grade (semesters not averaged) is %.1f or %f\n", nguwv2, nguwv2);
+			}
+			if(b6 == true && b8 == true)
+			{
+				double ngwv2 = (ninerGPAVersion2+w+1)/19;
+					System.out.printf("Your wieghted GPA for 9th-11th grade (semesters not averaged) is %.1f or %f\n", ngwv2, ngwv2);
+			}
+		}
+		if(b4 == true)
+		{
+			double sevenerGPA = sevenGPA();
+			double sevenerGPAVersion2 = sevenGPAVersion2();
+			if(b5 == true && b7 == true)
+			{
+				double sguw = sevenerGPA/22;
+					System.out.printf("Your unwieghted GPA for 7th-11th grade (semesters averaged) is %.1f or %f\n", sguw, sguw);
+			}
+			if(b6 == true && b7 == true)
+			{
+				double sgw = (sevenerGPA+w+1)/22;
+					System.out.printf("Your wieghted GPA for 7th-11th grade (semesters averaged) is %.1f or %f\n", sgw, sgw);
+			}
+			if(b5 == true && b8 == true)
+			{
+				double sguwv2 = sevenerGPAVersion2/22;
+					System.out.printf("Your unwieghted GPA for 7th-11th grade (semesters not averaged) is %.1f or %f\n", sguwv2, sguwv2);
+			}
+			if(b6 == true && b8 == true)
+			{
 			double sgwv2 = (sevenerGPA+w+1)/22;
 				System.out.printf("Your wieghted GPA for 7th-11th grade (semesters averaged) is %.1f or %f\n", sgwv2, sgwv2);
+			}
+		}
 		if(b5 == false && b6 == false && b7 == false && b8 == false)
 		{
 			System.out.println("You have selected to print nothing");
 		}
-		}
-		else
+		if(b1 == false && b2 == false && b3 == false && b4 == false)
 		{
 			System.out.println("You have selected to print nothing");
 		}
@@ -166,13 +197,79 @@ public class gpaFinal
 		Scanner in = new Scanner(System.in);
 		System.out.println(first + " \"a\" for yes and \"b\" for no.");
 		String a = in.next();
-		if(a.equals(a))
+		if(a.equals("a"))
 		{
 			second = true;
+			count++;
+			if(count == 1)
+			{
+				b1 = second;
+			}
+			if(count == 2)
+			{
+				b2 = second;
+			}
+			if(count == 3)
+			{
+				b3 = second;
+			}
+			if(count == 4)
+			{
+				b4 = second;
+			}
+			if(count == 5)
+			{
+				b5 = second;
+			}
+			if(count == 6)
+			{
+				b6 = second;
+			}
+			if(count == 7)
+			{
+				b7 = second;
+			}
+			if(count == 8)
+			{
+				b8 = second;
+			}
 		}
 		else
 		{
 			second = false;
+			count++;
+			if(count == 1)
+			{
+				b1 = second;
+			}
+			if(count == 2)
+			{
+				b2 = second;
+			}
+			if(count == 3)
+			{
+				b3 = second;
+			}
+			if(count == 4)
+			{
+				b4 = second;
+			}
+			if(count == 5)
+			{
+				b5 = second;
+			}
+			if(count == 6)
+			{
+				b6 = second;
+			}
+			if(count == 7)
+			{
+				b7 = second;
+			}
+			if(count == 8)
+			{
+				b8 = second;
+			}
 		}
 	}
 }
