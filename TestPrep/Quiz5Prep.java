55f1c5080c6957b9d6cbd746e3d288befb0a5da9
DerekRusher <derekrusher3@gmail.com>
2016-11-03 10:59:18 -0700

    did some test prep

--- TestPrep/Quiz5Prep.java
+++ TestPrep/Quiz5Prep.java
@@ -29,29 +29,39 @@ else
 	System.out.println("the bond is covalent");
 }
 
+KINDA FALSE, NEEDS A RETURN VALUE.
+
 //Question 2:
 D.
+CORRECT
 
 //Question 3:
 D.
+CORRECT
 
 //Question 4:
 A.
+CORRECT
 
 //Question 5: 
 D.
+ANSWER IS C - FOR ALL FALSES YOU MUST USE THE OPPOSITE OF OR.
 
 //Question 6:
 D.
+CORRECT
 
 //Question 7:
 A.
+CORRECT
 
 //Question 8: 
 C.
+CORRECT
 
 //Question 9:
 C.
+ANSWER IS D - THIS WAS YOU BEING STUPID.
 
 //Question 10:
 if(total == MAX)
@@ -68,5 +78,5 @@ if(total == MAX)
 else
    System.out.println("total is not equal to MAX");
 
-
+CORRECT
 
---
+++ TestPrep/Quiz5Prep2.java
@@ -0,0 +1,11 @@
+//QUESTION 1
+
+public static String calcBond(double one, double two)
+{
+	if(one > two)
+	{
+		if (one - two >= )
+	}
+} 
+
+NOPE TOO LATE NOT ENOUGH TIME
