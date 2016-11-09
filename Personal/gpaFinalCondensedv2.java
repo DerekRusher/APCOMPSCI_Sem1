8a05caa81f26d40611e524571f3376aabb18729a
DerekRusher <derekrusher3@gmail.com>
2016-11-09 12:12:23 -0800

    made condensed 2, continued condensing

--- Personal/gpaFinalCondensedv2.java
+++ Personal/gpaFinalCondensedv2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class gpaFinalCondensed
+public class gpaFinalCondensedv2
 {
 	static double ph;
 	static double es;
@@ -17,10 +17,16 @@ public class gpaFinalCondensed
 	static boolean b6;
 	static boolean b7;
 	static boolean b8;
+	static double pGPA;
+	static double tenerGPA;
+	static double tenerGPAVersion2;
+	static double ninerGPA;
+	static double ninerGPAVersion2;
+	static double sevenerGPA;
+	static double sevenerGPAVersion2;
 	public static void main(String[]args)
 	{
 		Scanner in = new Scanner(System.in);
-		
 		ph = printX("What is your current grade in AP Physics 1?", ph);
 		es = printX("What is your current grade in ESD?", es);
 		sp = printX("What is your current grade in Spanish 2?", sp);
@@ -36,19 +42,19 @@ public class gpaFinalCondensed
 		b6 = print("Do you want to see weighted GPA?", b6);
 		b7 = print("Do you want to see semesters averaged(v1)?", b7);
 		b8 = print("Do you want to see semesters unaveraged(v2)?", b8);
-		double pGPA = calcTotal(0);
-		double tenerGPA = calcTotal(4 + 4 + 4 + 4 + 3.5 + 3);
-		double tenerGPAVersion2 = calcTotal(4 + 4 + 4 + 4 + 3 + 3);
-		double ninerGPA = calcTotal(4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3.5 + 3);
-		double ninerGPAVersion2 = calcTotal(4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3 + 3);
-		double sevenerGPA = calcTotal(4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3.5 + 3);
-		double sevenerGPAVersion2 = calcTotal(4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3 + 3);	
+		pGPA = calcTotal(0);
+		tenerGPA = calcTotal(4 + 4 + 4 + 4 + 3.5 + 3);
+		tenerGPAVersion2 = calcTotal(4 + 4 + 4 + 4 + 3 + 3);
+		ninerGPA = calcTotal(4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3.5 + 3);
+		ninerGPAVersion2 = calcTotal(4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3 + 3);
+		sevenerGPA = calcTotal(4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3.5 + 3);
+		sevenerGPAVersion2 = calcTotal(4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3 + 3);	
 		if(b1 == true)
 		{
 			if(b5 == true)
 				System.out.printf("Your unwieghted GPA for this year is %.1f or %f\n", (pGPA/7), (pGPA/7));
 			if(b6 == true)
-				System.out.printf("Your wieghted GPA is for this year is %.1f or %f\n", ((pGPA + 5)/7), ((pGPA + 5)/7));
+				System.out.printf("Your wieghted GPA for this year is %.1f or %f\n", ((pGPA + 5)/7), ((pGPA + 5)/7));
 		}
 		if(b2 == true)
 		{
@@ -86,17 +92,30 @@ public class gpaFinalCondensed
 		if((b5 == false && b6 == false && b7 == false && b8 == false) || (b1 == false && b2 == false && b3 == false && b4 == false))
 			System.out.println("You have selected to print nothing");
 	}
-	
-	public static double calcPoints(double z)
+	/*public static String con(boolean ah, String a, double z, double y, double x, double w)
+	{
+		if(ah == true)
+		{
+			if(b5 == true && b7 == true)
+				System.out.printf("Your unwieghted GPA for " + a + "th-11th grade (semesters averaged) is %.1f or %f\n", z, z);
+			if(b6 == true && b7 == true)
+				System.out.printf("Your wieghted GPA for " + a + "th-11th grade (semesters averaged) is %.1f or %f\n", y, y);
+			if(b5 == true && b8 == true)
+				System.out.printf("Your unwieghted GPA for " + a + "th-11th grade (semesters not averaged) is %.1f or %f\n", x, x);
+			if(b6 == true && b8 == true)
+				System.out.printf("Your wieghted GPA for " + a + "th-11th grade (semesters not averaged) is %.1f or %f\n", w, w);
+		}
+	}*/
+	public static double calcPoints(double m)
 	{
 		double one = 0.0;
-		if (z >= 90)
+		if (m >= 90)
 			one = 4.0;
-		else if (z >= 80)
+		else if (m >= 80)
 			one = 3.0;
-		else if (z >= 70)
+		else if (m >= 70)
 			one = 2.0;
-		else if (z >= 60)
+		else if (m >= 60)
 			one = 1.0;
 		else
 			one = 0.0;
