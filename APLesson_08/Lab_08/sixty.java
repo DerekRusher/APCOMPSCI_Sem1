124f0a6fa8d598c336d13e93aa2b0d5590f87d18
DerekRusher <derekrusher3@gmail.com>
2016-11-09 11:40:48 -0800

    finished lab_08

--- APLesson_08/Lab_08/sixty.java
+++ APLesson_08/Lab_08/sixty.java
@@ -6,5 +6,18 @@ public class sixty
 	{
 		Scanner in = new Scanner(System.in);
 		System.out.println("Please insert a word: ");
+		String one = in.next();
+		int two = one.length();
+		tree(one, 0, two); 
+	}
+	
+	public static String tree(String a, int start, int stop)
+	{
+		if(start <= stop)
+		{
+			System.out.printf("%20s\n", a.substring(0, start));
+			return tree(a, start+1, stop);
+		}
+		return a;
 	}
 }
