6b3c5d9025bbbc7edd269dcd8d1f89daf2bb24c9
DerekRusher <derekrusher3@gmail.com>
2016-11-29 12:13:40 -0800

    did part of Lab_10

---
+++ APLesson_10/Lab_10/incompleteEquation.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class incompleteEquation{
+	public static void main(String[]args){
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter an equation: ");
+		ArrayList<String>equation = new ArrayList<>(Arrays.asList(kb.nextLine()));
+		run(equation);
+	}
+	
+	public static void run(ArrayList<String>){
+		int i = 0;
+		//while(i < so )
+	}
+}
---
+++ APLesson_10/Lab_10/notPrime.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class notPrime{
+	static ArrayList<Integer> nums;
+	public static void main(String[]args){
+		Integer [] numbers = {2,6,8,9,10,12,13,15,17,24,55,66,78,77,79};
+		nums = new ArrayList<>(Arrays.asList(numbers));
+		remove();
+		System.out.println(nums);
+	}
+	static int gFactor(int n)
+	{
+		for(int i = 2; i < n; i++)
+		{
+			if(n % i == 0)
+				return 1;
+		}
+		return 0;
+	}
+	static void remove()
+	{
+		for(int i = 0; i < nums.size(); i++)
+		{
+			if(gFactor(nums.get(i)) == 0)
+				nums.remove(i);
+		}
+	}
+}
