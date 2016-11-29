97b00469252678f6d159418c00a8d6be0723e02d
DerekRusher <derekrusher3@gmail.com>
2016-11-29 10:36:27 -0800

    removed unneccisary spacing and got under 100 lines

--- Personal/gpaFinalCondensedv2.java
+++ Personal/gpaFinalCondensedv2.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
-
-public class gpaFinalCondensedv2
-{
+public class gpaFinalCondensedv2{
 	static double ph;
 	static double es;
 	static double sp;
@@ -24,8 +22,7 @@ public class gpaFinalCondensedv2
 	static double ninerGPAVersion2;
 	static double sevenerGPA;
 	static double sevenerGPAVersion2;
-	public static void main(String[]args)
-	{
+	public static void main(String[]args){
 		Scanner in = new Scanner(System.in);
 		ph = printX("What is your current grade in AP Physics 1?", ph);
 		es = printX("What is your current grade in ESD?", es);
@@ -49,23 +46,18 @@ public class gpaFinalCondensedv2
 		ninerGPAVersion2 = calcTotal(4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3 + 3);
 		sevenerGPA = calcTotal(4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3.5 + 3);
 		sevenerGPAVersion2 = calcTotal(4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 4 + 3 + 3);	
-		if(b1 == true)
-		{
+		if(b1 == true){
 			if(b5 == true)
 				System.out.printf("Your unwieghted GPA for this year is %.1f or %f\n", (pGPA/7), (pGPA/7));
 			if(b6 == true)
-				System.out.printf("Your wieghted GPA for this year is %.1f or %f\n", ((pGPA + 5)/7), ((pGPA + 5)/7));
-		}
+				System.out.printf("Your wieghted GPA for this year is %.1f or %f\n", ((pGPA + 5)/7), ((pGPA + 5)/7));}
 		con(b2, "10", (tenerGPA/13), ((tenerGPA+6)/13), (tenerGPAVersion2/13), ((tenerGPAVersion2+6)/13));
 		con(b3, "9", (ninerGPA/19), ((ninerGPA+6)/19), (ninerGPAVersion2/19), ((ninerGPAVersion2+6)/19));
 		con(b4, "7", (tenerGPA/22), ((tenerGPA+6)/22), (tenerGPAVersion2/22), ((tenerGPAVersion2+6)/22));
 		if((b5 == false && b6 == false && b7 == false && b8 == false) || (b1 == false && b2 == false && b3 == false && b4 == false))
-			System.out.println("You have selected to print nothing");
-	}
-	public static void con(boolean ah, String a, double z, double y, double x, double w)
-	{
-		if(ah == true)
-		{
+			System.out.println("You have selected to print nothing");}
+	public static void con(boolean ah, String a, double z, double y, double x, double w){
+		if(ah == true){
 			if(b5 == true && b7 == true)
 				System.out.printf("Your unwieghted GPA for " + a + "th-11th grade (semesters averaged) is %.1f or %f\n", z, z);
 			if(b6 == true && b7 == true)
@@ -73,11 +65,8 @@ public class gpaFinalCondensedv2
 			if(b5 == true && b8 == true)
 				System.out.printf("Your unwieghted GPA for " + a + "th-11th grade (semesters not averaged) is %.1f or %f\n", x, x);
 			if(b6 == true && b8 == true)
-				System.out.printf("Your wieghted GPA for " + a + "th-11th grade (semesters not averaged) is %.1f or %f\n", w, w);
-		}
-	}
-	public static double calcPoints(double m)
-	{
+				System.out.printf("Your wieghted GPA for " + a + "th-11th grade (semesters not averaged) is %.1f or %f\n", w, w);}}
+	public static double calcPoints(double m){
 		double one = 0.0;
 		if (m >= 90)
 			one = 4.0;
@@ -89,24 +78,15 @@ public class gpaFinalCondensedv2
 			one = 1.0;
 		else
 			one = 0.0;
-		return one;
-	}
-	
-	public static double calcTotal(double lol)
-	{
-		return lol + calcPoints(ph) + calcPoints(es) + calcPoints(sp) + calcPoints(cs) + calcPoints(im) + calcPoints(el) + calcPoints(us);
-	}
-	
-	public static double printX(String primera, double secunda)
-	{
+		return one;}
+	public static double calcTotal(double lol){
+		return lol + calcPoints(ph) + calcPoints(es) + calcPoints(sp) + calcPoints(cs) + calcPoints(im) + calcPoints(el) + calcPoints(us);}
+	public static double printX(String primera, double secunda){
 		Scanner in = new Scanner(System.in);
 		System.out.println(primera);
 		secunda = in.nextDouble();
-		return secunda;
-	}
-	
-	public static boolean print(String first, boolean second)
-	{
+		return secunda;}
+	public static boolean print(String first, boolean second){
 		Scanner in = new Scanner(System.in);
 		System.out.println(first + " \"a\" for yes and anything else for no.");
 		String a = in.next();
@@ -114,6 +94,4 @@ public class gpaFinalCondensedv2
 			second = true;
 		else
 			second = false;
-		return second;
-	}
-}
+		return second;}}
