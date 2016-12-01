634f57c024c4da6dce7bef28974f270f384bf2d6
DerekRusher <derekrusher3@gmail.com>
2016-12-01 11:45:32 -0800

    finished Lab_10

--- APLesson_10/Lab_10/incompleteEquation.java
+++ APLesson_10/Lab_10/incompleteEquation.java
@@ -4,13 +4,34 @@ import java.util.Arrays;
 public class incompleteEquation{
 	public static void main(String[]args){
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Please enter an equation: ");
-		ArrayList<String>equation = new ArrayList<>(Arrays.asList(kb.nextLine()));
+		System.out.println("Please enter an expression (with spaces): ");
+		String a = kb.nextLine();
+		ArrayList<String>equation = new ArrayList<>(Arrays.asList(a.split(" ")));
 		run(equation);
 	}
-	
-	public static void run(ArrayList<String>){
+		
+	public static void run(ArrayList<String> equation){
 		int i = 0;
-		//while(i < so )
+		while(i < equation.size()){
+			if(equation.get(i).equals("*") || equation.get(i).equals("/")){
+				if(equation.get(i).equals("*"))
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) * (Integer.parseInt(equation.get(i+1)))));
+				else
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) / (Integer.parseInt(equation.get(i+1)))));
+				equation.remove(i-1);
+				equation.remove(i);
+			}
+			else if(equation.get(i).equals("+") || equation.get(i).equals("-")){
+				if(equation.get(i).equals("+"))
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) + (Integer.parseInt(equation.get(i+1)))));
+				else
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) - (Integer.parseInt(equation.get(i+1)))));
+				equation.remove(i-1);
+				equation.remove(i);
+			}
+			else
+				i++;
+		}
+		System.out.println(equation);
 	}
 }
